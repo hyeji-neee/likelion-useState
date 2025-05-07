05d2a3f3ababb4584b644cee5eb96fd18c2071d3
hyeji-neee <tlsgmlsk123@naver.com>
2025-05-07 20:20:04 +0900

    To-Do List 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Counter from "./Counter";
+import TodoList from "./TodoList";
 
 const App = () => {
-  return <Counter />;
+  return <TodoList />;
 };
 
 export default App;
